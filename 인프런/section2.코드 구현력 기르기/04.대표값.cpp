eeb3c3077908dd355413774b1442345e95bf45d1
Hyegyeong <gju06061@naver.com>
2023-12-19 19:09:20 +0900

    인프런 section2 04 문제 풀이

---
+++ 인프런/section2.코드 구현력 기르기/04.대표값.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> score(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> score[i];
+    }
+    double sum = accumulate(score.begin(), score.end(), 0.0);
+    // 1. 평균 구하기
+    int avg;
+    avg = round(sum / n);
+    cout << avg << " ";
+
+    // 2. 평균에 가장 가까운 학생은 몇 번째 학생인지 출력
+    int answerGap = avg;
+    int answer = 0;
+    for (int i = 0; i < n; ++i) {
+        int gap = abs(score[i] - avg);
+        if ((gap < answerGap) || ((gap == answerGap) && (score[i] > score[answer]))) {
+            answerGap = gap;
+            answer = i;
+        }
+    }
+    cout << answer + 1;
+}
