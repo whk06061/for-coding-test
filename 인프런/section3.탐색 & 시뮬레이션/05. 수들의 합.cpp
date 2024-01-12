9b35b8400e363c32fc35823def5a9f5ae70f1215
Hyegyeong <gju06061@naver.com>
2024-01-13 01:34:27 +0900

    인프런 section3 05 문제 풀이 수정

--- 인프런/section3.탐색 & 시뮬레이션/05. 수들의 합.cpp
+++ 인프런/section3.탐색 & 시뮬레이션/05. 수들의 합.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
     int N, M;
-    int answer = 0;
     cin >> N >> M;
     vector<int> a(N);
+
     for (int i = 0; i < N; ++i) {
         cin >> a[i];
     }
-    int s = 0, e = 0;
-    int current = 0;
-    while (s <= e && e <= N) {
-        if (current < M) {
-            current += a[e++];
-        } else {
-            if (current == M) answer++;
-            current -= a[s++];
+
+    int s = 0, e = 1;
+    int current = a[0];
+    int answer = 0;
+
+    while(true){
+        if(current < M){
+            if (e < N){
+                current += a[e++];
+            }else break;
+        }else{
+            if (current == M) answer ++;
+            current -=a[s++];
         }
     }
-    while (s <= N) {
-        if (current < M) break;
-        if (current == M) answer++;
-        current -= a[s++];
-    }
+
     cout << answer;
 }
